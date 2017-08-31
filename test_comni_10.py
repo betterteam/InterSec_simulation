ac465c88b0ab27f2871f1800b57e5feb378ca469
Wen Gu <wen.gu@uah.es>
2017-08-31 21:44:55 +0900

    test UDP successfully

--- test_comni_10.py
+++ test_comni_10.py
@@ -1,17 +1,32 @@
 
 from datetime import datetime
 import socket
+import struct
 
 server_address = ('localhost', 6789)
 max_size = 4096
 
-print('starting the server at', datetime.now())
-print('waiting for a client to call.')
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server.bind(server_address)
+STOP_CHAT = True
 
-data, client = server.recvfrom(max_size)
+while STOP_CHAT:
+    check = 0
 
-print('At', datetime.now(), client, 'said', data)
-server.sendto(b'Are you talking to me?', client)
+    print('starting the server at', datetime.now())
+    print('waiting for a client to call.')
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server.bind(server_address)
+
+    data, client = server.recvfrom(max_size)
+
+    data = struct.unpack("i", data)
+    print(data)
+
+    if data[0] % 2 == 1:
+       check = 1
+
+    print('At', datetime.now(), client, 'said', data)
+
+    str = struct.pack("i", check)
+    server.sendto(str, client)
+
 server.close()
--- test_comni_11.py
+++ test_comni_11.py
@@ -1,12 +1,26 @@
 import socket
 from datetime import datetime
+import struct
 
 server_address = ('localhost', 6789)
 max_size = 4096
 
-print('Starting the client at', datetime.now())
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client.sendto(b'Hey!', server_address)
-data, server = client.recvfrom(max_size)
-print('At', datetime.now(), server, 'said', data)
+i = 1
+
+while i < 100:
+
+    print('Starting the client at', datetime.now())
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    mes = struct.pack("i", i)
+
+    client.sendto(mes, server_address)
+    data, server = client.recvfrom(max_size)
+
+    data = struct.unpack("i", data)
+
+    print('At', datetime.now(), server, 'said', data)
+
+    i += 1
+
 client.close()
